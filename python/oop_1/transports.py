5579248ec1bad23caa9e14064167efd9f5e655be
Pavel Braha <strangelancer@gmail.com>
2021-05-06 07:40:49 +0300

    implemented multiple inheritance

--- python/oop_1/transports.py
+++ python/oop_1/transports.py
@@ -1,9 +1,9 @@
 """
 Задание: https://classroom.google.com/c/Mjk0OTc3NDgyMzU1/a/MzM1MzAyOTU5NTg2/details
-- попрактиковаться в создании классов с нуля и наследовании от других классов
-- создать класс Transport, подумать об атрибутах и методах, дополнить класс ими
-- подумать и реализовать класс наследники класса Transport(минимум 4),
-- переопределить методы и атрибуты для каждого класса
+- practice creating classes from scratch and inheriting from other classes
+- create class named Transport, think about attributes and methods, add them to the class
+- think over and implement the class inheriting from the Transport class (at least 4),
+- override methods and attributes for each class
 """
 import datetime
 
@@ -11,19 +11,23 @@ import datetime
 class Transport:
     """class Transport"""
 
+    # count of all avto
+    count_of_transport = 0
+
     def __init__(self, model: str, year: int, segment: str) -> None:
         self.model = model
         self.year = year
         self.segment = segment
         self.average_distans_peryear = 6356
         self.today = datetime.datetime.today().year
+        Transport.count_of_transport += 1
 
     def run(self):
-        print("Машина завелась")
+        print("The car has already started")
 
     def mileage(self):
         print(
-            f"Ваш {self.model} проехал около \
+            f"Your {self.model} has been driven about \
                 {self.average_distans_peryear * (self.today - self.year)} км"
         )
 
@@ -41,7 +45,7 @@ class Volkswagen(Transport):
 
     def run(self):
         super().run()
-        print("Теперь можно ехать")
+        print("Now you can drive")
 
 
 class Audi(Volkswagen):
@@ -53,27 +57,53 @@ class Audi(Volkswagen):
 
     def mileage(self):
         print(
-            f"Ваш {self.model} проехал около \
+            f"Yours {self.model} has been driven about \
                 {self.average_distans_peryear * (self.today - self.year)} км. \n \
-                Cегмент: {self.segment}, коробка {self.transmition}"
+                Segment: {self.segment}, transmission {self.transmition}"
         )
 
 
-class Seat(Volkswagen):
+class Inspection(Transport):
+    """
+    class Inspection
+    checks the condition of the transport
+    """
+
+    def check_auto(self):
+        a = self.average_distans_peryear * (self.today - self.year)
+        if a > 80000:
+            print(
+                """U must CO-2 routine maintenance, and then CO-1 \
+                maintenance no more than once every 8 thousand kilometers)"""
+            )
+        else:
+            print("Don't worry, all is well.")
+
+
+class Seat(Volkswagen, Inspection):
     """class Seat"""
 
     def run(self):
         super().run()
         print(
-            f"А точнее ехать на своей тачке марки {self.__class__.__name__ +' '+self.model} "
+            f"(more specifically) - to drive your own car of the brand {self.__class__.__name__ +' '+self.model} "
         )
 
 
 # for teacher
-# mers = Audi("A4", 2012, "premium", "avtomat")
-# mers.mileage()
-# mers.run()
+mers = Audi("A4", 2012, "premium", "avtomat")
+mers.mileage()
+mers.run()
+print(mers.count_of_transport)
 
-# seat = Seat("Leon", 2009, "premium")
-# seat.mileage()
-# seat.run()
+
+seat = Seat("Leon", 2009, "B")
+seat.mileage()
+seat.run()
+print(seat.count_of_transport)
+
+seat2 = Seat("Ibiza", 2006, "C")
+seat2.mileage()
+seat2.run()
+seat2.check_auto()
+print(seat.count_of_transport)
