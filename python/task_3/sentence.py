1abf2cf1cff3e27588224f4dbe26e11c7c2f2438
Pavel Braha <strangelancer@gmail.com>
2021-05-14 19:53:56 +0300

    edited request

--- python/task_3/sentence.py
+++ python/task_3/sentence.py
@@ -27,8 +27,8 @@ class Sentence:
 
     def __init__(self, text: str):
         if isinstance(text, str) and text[-1] in ["?", ".", "!"]:
-            self.text = [i for i in re.findall(r"\w+", text)]
-            self.chars = [i for i in re.findall(r"[.,!?;:]", text)]
+            self.text = [i for i in re.findall(r"[^\d\s.,!?/(-/)=*&^%$#@~`;:]+", text)]
+            self.chars = [i for i in re.findall(r"[\d.,!?/(-/)=*&^%$#@~`;:]", text)]
         elif not isinstance(text, str):
             raise TypeError("U must type a string")
         else:
@@ -51,12 +51,13 @@ class Sentence:
     @property
     def words(self):
         "return words generators"
-        return (i for i in self.text)
+        return self._words()
 
     @property
     def other_chars(self):
         "return chars generators"
-        return (i for i in self.chars)
+        return [i for i in self.chars]
 
 
-obj1 = Sentence("Не помню, :;не люблю fuckkkkkkkk!!!")
+obj1 = Sentence("Не пом-ню, - и :;не (2люблю fuckkkkkkkk!!!!!!")
+print(obj1[:])
