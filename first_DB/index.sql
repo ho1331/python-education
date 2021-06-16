a93515e9de65de996f3d9d071c6ed17a5a3795ca
Pavel Braha <strangelancer@gmail.com>
2021-06-16 03:53:06 +0300

    added join query

--- first_DB/index.sql
+++ first_DB/index.sql
@@ -27,3 +27,20 @@ WHERE category_id = 2  GROUP BY category_id HAVING COUNT(product_title) > 180; -
 
 
 DROP INDEX idx_products_category_id,idx_cart_product_product_id,idx_carts_subtotal;
+
+
+--with join
+CREATE INDEX idx_orders ON orders(order_status_id);
+
+EXPLAIN
+SELECT orders.total,
+    status_name,
+    last_name clients
+FROM carts
+    JOIN users USING(user_id)
+    JOIN orders USING(cart_id)
+    JOIN order_statuses USING(order_status_id)
+WHERE orders.total > 75
+    AND order_status_id = 4;
+
+DROP INDEX idx_orders;
